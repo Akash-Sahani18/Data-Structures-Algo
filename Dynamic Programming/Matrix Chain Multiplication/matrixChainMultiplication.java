fc649fddb9310ab05c84179cdbc1a6069b9b176d
Akash8012 <sahaniaakash563@gmail.com>
2026-09-05 23:46:20 +0530

    Matrix Chain Multiplication Pattern Problem

---
+++ Dynamic Programming/Matrix Chain Multiplication/matrixChainMultiplication.java
@@ -0,0 +1,22 @@
+
+class matrixChainMultiplication {
+
+    public static int chainMul(int nums[], int i, int j) {
+
+        if (i + j == 0) {
+            return 0;
+        }
+        int curr = Integer.MAX_VALUE;
+        for (int k = i; k < j; k++) {
+            curr = chainMul(nums, i, k) + chainMul(nums, k + 1, j);
+        }
+        return curr;
+    }
+
+    public static void main(String args[]) {
+        int nums[] = {2, 1, 3, 4};
+        int i = 0;
+        int j = nums.length;
+        System.out.println(chainMul(nums, i, j) + " ");
+    }
+}
