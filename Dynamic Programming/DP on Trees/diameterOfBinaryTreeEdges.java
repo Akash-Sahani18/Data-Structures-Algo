4c77da3fbf549b8c5b3ee19651c337fa3ee885ec
Akash8012 <sahaniaakash563@gmail.com>
2026-09-09 14:39:09 +0530

    DP on Trees

---
+++ Dynamic Programming/DP on Trees/diameterOfBinaryTreeEdges.java
@@ -0,0 +1,51 @@
+
+class Node {
+
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int val) {
+        this.left = null;
+        this.right = null;
+        data = val;
+    }
+}
+//Diameter of Binary Tree in Edges
+
+class diameterOfBinaryTreeEdges {
+
+    static int res = 0;
+
+    public static int solve(Node root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int lheight = solve(root.left);
+        int rheight = solve(root.right);
+
+        if (rheight + lheight > res) {
+            res = rheight + lheight;
+        }
+        return 1 + Math.max(rheight, lheight);
+    }
+
+    public static int calculate(Node root) {
+        solve(root);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+        root.right.left = new Node(60);
+        root.right.right = new Node(60);
+        System.out.println(calculate(root) + " ");
+
+    }
+}
